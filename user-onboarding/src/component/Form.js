1207e5c6f1b9867cf153a604d51b8220bf2d21bd
Brian Kubes <briankubes@gmail.com>
2020-09-16 15:23:27 -0700

    working, but need to get axios.post set up

--- user-onboarding/src/component/Form.js
+++ user-onboarding/src/component/Form.js
@@ -1,20 +1,71 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as yup from "yup";
 import axios from "axios";
 
 export default function Form() {
+  //state for users
   const [userData, setUserData] = useState({
     name: "",
     email: "",
   });
 
+  //state for error messages
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+  });
+
+  //state for toggle submit button
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  // toggles submit button when userData is valid
+  useEffect(() => {
+    formSchema.isValid(userData).then((valid) => {
+      setButtonDisabled(!valid);
+    });
+  }, [userData]);
+
   const onChange = (e) => {
-    const { name, value } = e.target;
+    e.persist();
+    //destructure the event objects
+    const { name, value, checked, type } = e.target;
+    //prepare userData to be validate based on 'value' or 'checked' etc
+    // const prepareUserData = {
+    //   ...userData,
+    //   [name]: type === "checkbox" ? checked : value,
+    // };
+    //send these destructured props to get validated
+    validateChange(name, value);
+    //update userData with 'prepareUserData' regardless if it passes validation
     setUserData({ ...userData, [name]: value });
   };
 
+  const validateChange = (name, value) => {
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then((valid) => {
+        setErrors({ ...errors, [name]: "" });
+      })
+      .catch((err) => {
+        setErrors({ ...errors, [name]: err.errors[0] });
+      });
+  };
+
+  const formSchema = yup.object().shape({
+    name: yup.string().required("your name is required"),
+    email: yup
+      .string()
+      .email("must be valid email")
+      .required("your email is required"),
+  });
+
+  const submit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={submit}>
       <label htmlFor="name">
         Name:
         <input
@@ -24,6 +75,7 @@ export default function Form() {
           value={userData.name}
           onChange={onChange}
         ></input>
+        {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
       </label>
       <label htmlFor="email">
         Email:
@@ -34,7 +86,11 @@ export default function Form() {
           value={userData.email}
           onChange={onChange}
         ></input>
+        {errors.email.length > 0 ? (
+          <p className="error">{errors.email}</p>
+        ) : null}
       </label>
+      <button disabled={buttonDisabled}>Submit</button>
     </form>
   );
 }
