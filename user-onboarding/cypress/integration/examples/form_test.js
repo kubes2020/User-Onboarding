b4c0e85b95c0cc5045a46cdf417ebc7e079f09a6
Brian Kubes <briankubes@gmail.com>
2020-09-17 12:42:40 -0700

    getting cypress set up

---
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -0,0 +1,8 @@
+describe("Test form", function () {
+  beforeEach(function () {
+    cy.visit("http://localhost:3000/");
+  });
+  it("Add test to inputs and submit form", function () {
+    cy.get('input[name="name"]').type("Brian");
+  });
+});
